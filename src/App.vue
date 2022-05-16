571baec7e9f8b00559a923a70020211fce1bfe95
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-05-16 10:41:01 +0200

    scaffolding creato

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+
+    <app-header />
+
+    <app-main />
+
+    <app-footer />
+
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./styles/generals";
+#app {
+
+}
+</style>
